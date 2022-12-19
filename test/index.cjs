5fd0e3384ceb1f1d2082a4a497569b250d3c8186
Lew <yl617341586@163.com>
2022-12-19 23:20:58 +0800

    refactor: 💡 添加测试代码

--- test/index.cjs
+++ test/index.cjs
@@ -1,3 +1,6 @@
 const { generateTypeFile } = require('../lib');
 const swaggerJson = require('./swagger.json');
+const { resolve } = require('path');
+const { cwd } = require('process');
 generateTypeFile(swaggerJson);
+// generateTypeFile(swaggerJson, resolve(cwd(), 'test.ts'));
