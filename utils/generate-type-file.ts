fc5c57736125a512b01de463bb4b6cf7bfabcebb
yaol <yaol@jiguang.cn>
2022-12-30 11:50:42 +0800

    refactor: 💡 修改路径规则

--- utils/generate-type-file.ts
+++ utils/generate-type-file.ts
@@ -4,7 +4,7 @@ import { writeFileSync } from 'fs';
 import { cwd } from 'process';
 import { resolve } from 'path';
 export default (json: Swagger, output?: string) => {
-  const path = output ? resolve(output) : resolve(cwd(), 'type.ts');
+  const path = output ? output : resolve(cwd(), 'type.ts');
   writeFileSync(path, generateType(json), {
     encoding: 'utf-8',
   });
