0c8afc7d5fdd0f1da51b964ac8b0c03917f856b3
Lew <yl617341586@163.com>
2023-08-15 16:28:59 +0800

    fix: 🐛 修复多层目录，文件创建失败的问题

--- src/generate-type-file.ts
+++ src/generate-type-file.ts
@@ -1,19 +1,26 @@
-import { writeFileSync, constants, accessSync } from 'fs';
+import { writeFileSync, constants, accessSync, mkdirSync } from 'fs';
 import { cwd } from 'process';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
 import generateType from './generate-type';
 import { OpenApi } from '../openapi';
 export default async (json: OpenApi, output = resolve(cwd(), 'type.ts')) => {
+  const path = dirname(output);
   const data = await generateType(json);
   const write = () => {
     writeFileSync(output, data, { encoding: 'utf-8' });
     console.log(`[Swagger2TSFile]: 文件生成路路径 ${output}`);
   };
   try {
+    accessSync(path, constants.F_OK);
+  } catch (e: any) {
+    if (e.code === 'ENOENT') mkdirSync(path, { recursive: true });
+    else return console.error(`[Swagger2TSFile]: ${e}`);
+  }
+  try {
     accessSync(output, constants.F_OK);
     write();
   } catch (e: any) {
-    if (e.code === 'ENOENT') return write();
-    console.error(`[Swagger2TSFile]: ${e}`);
+    if (e.code === 'ENOENT') write();
+    else return console.error(`[Swagger2TSFile]: ${e}`);
   }
 };
--- test/index.cjs
+++ test/index.cjs
@@ -2,5 +2,5 @@ const { generateTypeFile, generateType } = require('../lib');
 const swaggerJson = require('./swagger.json');
 const { resolve } = require('path');
 const { cwd } = require('process');
-generateTypeFile(swaggerJson);
-// generateTypeFile(swaggerJson, resolve(cwd(), 'test.ts'));
+// generateTypeFile(swaggerJson);
+generateTypeFile(swaggerJson, resolve(cwd(), 'api', 'test.ts'));
